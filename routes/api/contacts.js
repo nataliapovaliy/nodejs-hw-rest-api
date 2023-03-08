f4e59697e8467f6a0c2f4e6f6e09fc9a99a4fb83
nataliapovaliy <natalia.bg1125@gmail.com>
2023-03-08 20:59:42 +0200

    validation

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const contacts = require("../../models/contacts");
 const router = express.Router()
+const Joi = require('joi');
 
 router.get('/', async (req, res, next) => {
   try {
     const data = await contacts.listContacts();
-    res.json(data);
+    res.status(201).json(data);
   } catch (error) { next(error); }
 })
 
@@ -21,6 +22,17 @@ router.get('/:contactId', async (req, res, next) => {
 })
 
 router.post('/', async (req, res, next) => {
+  const schema = Joi.object({
+    name: Joi.string().alphanum().min(3).max(30).required(),
+    email: Joi.string()
+      .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
+    phone: Joi.string().required()
+  })
+  const validationResult = schema.validate(req.body);
+  if (validationResult.error) {
+    return res.status(400).json({status: validationResult.error.details})
+  }
+
   try {
     const newContacts = await contacts.addContact(req.body);
     res.status(201).json(newContacts);
@@ -36,6 +48,17 @@ router.delete('/:contactId', async (req, res, next) => {
 })
 
 router.put('/:contactId', async (req, res, next) => {
+  const schema = Joi.object({
+    name: Joi.string().alphanum().min(3).max(30).optional(),
+    email: Joi.string()
+      .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).optional(),
+    phone: Joi.string().optional()
+  })
+  const validationResult = schema.validate(req.body);
+  if (validationResult.error) {
+    return res.status(400).json({status: validationResult.error})
+  }
+
   const { contactId } = req.params;
   const { name, email, phone } = req.body;
   try {
